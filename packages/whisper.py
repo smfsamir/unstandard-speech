1f3c3899428e8d11e5d77ef8a7910cb8c44f8e84
Farhan Samir <f.samir@mail.utoronto.ca>
2025-10-23 13:10:15 -0400

    update model

--- packages/whisper.py
+++ packages/whisper.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 
 import torch
-from faster_whisper import WhisperModel
+# from faster_whisper import WhisperModel
 
 import sys
 import os
 from tempfile import NamedTemporaryFile
 from dotenv import dotenv_values
+from transformers import WhisperForConditionalGeneration, AutoProcessor
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from .audio import audio_array_to_wav_file
 
 _model_size = "large-v1"  # other options: large-v1, large-v2, large-v3
 _model = None
+
 HF_CACHE_DIR = dotenv_values(".env")["HF_CACHE_DIR"]
+
+class WhisperWrapper():
+
+    def __init__(self, model, processor):
+        self.model = model
+        self.processor = processor
+    
+    def transcribe_from_array(self, 
+                              wav_array, 
+                              language="en"):
+        input_features = self.processor(
+            wav_array, sampling_rate=16000, return_tensors="pt"
+        ).input_features.to('cuda')
+        generated_ids = self.model.generate(input_features, language='en')
+        transcription = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return transcription
 
 def get_model(model_size):
     global _model, _model_size
@@ -22,12 +40,9 @@ def get_model(model_size):
         return _model
 
     _model_size = model_size
-    if torch.cuda.is_available():
-        # Run on GPU with FP16
-        _model = WhisperModel(model_size, device="cuda", compute_type="float16", cache_dir=HF_CACHE_DIR)
-    else:
-        # Run on CPU with INT8
-        _model = WhisperModel(model_size, device="cpu", compute_type="int8")
+    generation_model = WhisperForConditionalGeneration.from_pretrained(f"openai/{_model_size}", cache_dir=HF_CACHE_DIR).to('cuda')
+    processor = AutoProcessor.from_pretrained(f"openai/{_model_size}", cache_dir=HF_CACHE_DIR)
+    _model = WhisperWrapper(generation_model, processor)
 
     return _model
 
@@ -43,9 +58,9 @@ def whisper_transcribe_timestamped(input_path, model="small", language="en"):
 
 
 def whisper_transcribe_from_array(wav_array, model="small", language="en"):
-    with NamedTemporaryFile(suffix=".wav") as f:
-        audio_array_to_wav_file(wav_array, f.name)
-        return whisper_transcribe(f.name, model=model, language=language)
+    return get_model(model).transcribe_from_array(
+        wav_array, language=language
+    )
 
 
 def whisper_transcribe_timestamped_from_array(wav_array, model="small", language="en"):
