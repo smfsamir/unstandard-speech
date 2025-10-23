7397b55de07446c1112f6a608842236845870da7
Farhan Samir <f.samir@mail.utoronto.ca>
2025-10-23 13:12:31 -0400

    update

--- packages/whisper.py
+++ packages/whisper.py
@@ -10,7 +10,6 @@ from dotenv import dotenv_values
 from transformers import WhisperForConditionalGeneration, AutoProcessor
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from .audio import audio_array_to_wav_file
 
 _model_size = "large-v1"  # other options: large-v1, large-v2, large-v3
 _model = None
@@ -61,22 +60,3 @@ def whisper_transcribe_from_array(wav_array, model="small", language="en"):
     return get_model(model).transcribe_from_array(
         wav_array, language=language
     )
-
-
-def whisper_transcribe_timestamped_from_array(wav_array, model="small", language="en"):
-    with NamedTemporaryFile(suffix=".wav") as f:
-        audio_array_to_wav_file(wav_array, f.name)
-        return whisper_transcribe_timestamped(f.name, model=model, language=language)
-
-
-
-def whisper_output_to_text(output, timestamped=False):
-    if hasattr(output, "text"):
-        return getattr(output, "text")
-    if type(output) == dict and "text" in output:
-        return output["text"]
-    segments, _ = output
-    if timestamped:
-        return " ".join(word.word for segment in segments for word in segment.words)
-    else:
-        return " ".join(segment.text for segment in segments)
